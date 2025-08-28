68959006d666482d50ea669396166456c10b0407
JustFr4nck <francyperry07@gmail.com>
2025-08-28 23:45:04 +0200

    some tests of backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const data = require("./config/db.json");
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// questo definisce un endpoint su localhost:3000/data che ritorna una stringa "hello world"
+app.get("/", (req, res) => {
+  res.send({ message: "ciao mondo!" }).status(200);
+});
+
+app.get("/m", (req, res) => {
+  res.json(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is online on localhost: ${PORT} `);
+});
